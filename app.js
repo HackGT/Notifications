eb4f6c8901a353126a050d281743351ae4e4bcc2
faizanv <faizan.virani.44@gmail.com>
2016-09-05 02:00:55 -0400

    Enabled CORS

--- app.js
+++ app.js
@@ -35,9 +35,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('views'));
 app.use(session({secret: config.secret,
-                 saveUninitialized: true,
-                 resave: true
-               }));
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', routes);
 app.use('/api', api);
